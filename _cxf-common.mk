df64ac731f0cfa80e8ea57d3b9aaf032ccaaff6b
Ryan V. Bissell <ryan@bissell.org>
2016-07-21 07:22:14 +0000

    Creation of _cxf-common.mk

---
+++ _cxf-common.mk
@@ -0,0 +1,186 @@
+#
+# Copyright (c) 2016, Ryan V. Bissell
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+# See the enclosed "LICENSE" file for exact license terms.
+#
+
+ifndef CXF_COMMON_INCLUDE_GUARD
+override CXF_COMMON_INCLUDE_GUARD:=1
+
+cxf_myname:=$(notdir $(lastword $(MAKEFILE_LIST)))
+ifndef CXFOUT
+$(error You must define 'CXFOUT' before including $(cxf_myname))
+endif
+$(CXFOUT):
+	@$(test) mkdir -p $(CXFOUT)
+
+
+.PHONY: clean
+
+
+# .d files are precious because they serve to speed up subsequent builds.
+# .i files are precious because the user specifically asked for them.
+.PRECIOUS: $(CXFOUT)/%.d $(CXFOUT)/%.i
+
+
+# TODO: these should be specific to the host OS
+override CXFOBJ:=o
+override CXFLIB:=a
+override CXFEXE:=
+
+
+# TODO: some of these may be specific to the host OS
+ifdef TEST
+    override test:=echo
+    override test2:=>/dev/null $(test)
+    override pipe:=\|
+    override redir:=\>
+    override indir:=\<
+    override append:=\>\>
+    ifeq ($(TEST),2)
+        override test2:=$(test)
+    endif
+else
+    override test:=
+    override test2:=
+    override pipe:=|
+    override redir:=>
+    override indir:=<
+    override append:=>>
+endif
+
+
+define cxf_reset_target =
+    $(eval override cxf_target:=$(1))
+    $(eval override undefine cxf_srcfiles)
+    $(eval override undefine cxf_objfiles)
+    $(eval override undefine cxf_depfiles)
+    $(eval override undefine cxf_libfiles)
+    $(eval override undefine cxf_linkfiles)
+    $(eval override undefine cxf_deptargets)
+    $(eval override CXF_LDFLAGS:=$(LDFLAGS))
+    $(eval override CXF_CPPFLAGS:=$(CPPFLAGS))
+    $(eval override CXF_CXXFLAGS:=$(CXXFLAGS))
+endef
+
+
+define _cxf_compile_c++ =
+	@echo "+++ [$$(notdir $(cxf_target))] $$(notdir $$<)"
+	@$(test) $(CXX) $(CXF_CPPFLAGS) $(CXF_CXXFLAGS) -c $$< -o $$@
+endef
+
+
+# this will generate source/header dependency info
+#    sed:      use FQPN for target
+# additionally, the following ensures that the build still succeeds
+# if source files get renamed while .d files exist:
+#    echo:     add blank line
+#    sed:      strip the target (up-to and including colon)
+#    sed:      remove any continuation backslash
+#    fmt -1:   list 'words' (files) one per line
+#    sed:      strip leading spaces
+#    sed:      add trailing colons (and blank lines)
+#    sed:      remove lines containing only a colon
+define _cxf_gen_dependencies =
+	@$(test2) $(CXX) -MM $(CXF_CPPFLAGS) $(CXF_CXXFLAGS) $$< $(redir)$(CXFOUT)/$(1).d
+	@$(test2) mv -f $(CXFOUT)/$(1).d $(CXFOUT)/$(1).d.tmp
+	@$(test2) sed -e 's|.*:|$(CXFOUT)/$(1).o:|' $(indir)$(CXFOUT)/$(1).d.tmp $(redir)$(CXFOUT)/$(1).d
+	@$(test2) echo "" $(append)$(CXFOUT)/$(1).d
+	@$(test2) sed -e 's/.*://' \
+	             -e 's/\\$$$$//' $(indir)$(CXFOUT)/$(1).d.tmp $(pipe) \
+	 $(test2) fmt -1 $(pipe) \
+	 $(test2) sed -e 's/^ *//' \
+	             -e 's/$$$$/:\n/' \
+	             -e 's/^:.*$$$$/\n/' $(append)$(CXFOUT)/$(1).d
+	@$(test2) $(RM) $(CXFOUT)/$(1).d.tmp
+endef
+
+
+define _cxf_gen_static_pattern_rule
+    $(1): $(CXFOUT)/%.$(CXFOBJ): $(2)/%$(3) | $(CXFOUT)
+	@$(call _cxf_compile_c++)
+	@$(call _cxf_gen_dependencies,$$*)
+endef
+
+
+define cxf_add_sources =
+    $(eval override _src:=$(wildcard $(1)/$(2)))
+    $(eval override _ext:=$(suffix $(2)))
+    $(eval override _obj:=$(subst $(_ext),.$(CXFOBJ),$(_src)))
+    $(eval override _obj:=$(subst $(1),$(CXFOUT),$(_obj)))
+    $(eval override cxf_srcfiles+= $(_src))
+    $(eval override cxf_objfiles+= $(_obj))
+    $(eval $(call _cxf_gen_static_pattern_rule,$(_obj),$(1),$(_ext)))
+    $(eval undefine _src)
+    $(eval undefine _ext)
+    $(eval undefine _obj)
+endef
+
+
+define cxf_lib_dependency =
+    $(eval override _lib:=$(cxf_$(1)_lib))
+    $(eval override cxf_deptargets+= $(1))
+    $(eval override cxf_linkfiles+= $(_lib))
+    $(eval undefine _lib)
+endef
+
+
+define _cxf_import_depfiles =
+    $(eval override cxf_depfiles:=$(cxf_objfiles:.$(CXFOBJ)=.d))
+    -include $(cxf_depfiles)
+endef
+
+
+define cxf_build_static_library =
+    $(eval $(_cxf_import_depfiles))
+    $(eval override cxf_$(cxf_target)_lib:=$(CXFOUT)/$(1).a)
+
+    $(cxf_$(cxf_target)_lib): $(cxf_objfiles)
+	@echo +++ [$(cxf_target)] Generating static library \'$$(notdir $$@)\'...
+	@$(test) $(AR) rcs $$@ $$^
+
+    $(cxf_target): $(CXF_DEPENDS) $(cxf_$(cxf_target)_lib)
+
+    .PHONY: _clean_$(cxf_target)
+    _clean_$(cxf_target):
+	@echo +++ [$(cxf_target)] Cleaning...
+	@$(test) $(RM) $(cxf_objfiles)
+	@$(test) $(RM) $(cxf_depfiles)
+	@$(test) $(RM) $(cxf_$(cxf_target)_lib)
+
+    clean:: _clean_$(cxf_target)
+	@[ -e $(CXFOUT) ] && $(test) rmdir --ignore-fail-on-non-empty $(CXFOUT)
+endef
+
+
+define cxf_build_executable =
+    $(eval $(_cxf_import_depfiles))
+    $(eval override cxf_program:=$(CXFOUT)/$(1)$(CXFEXE))
+
+    $(cxf_program): $(cxf_objfiles) $(cxf_linkfiles)
+	@echo +++ [$(cxf_target)] Generating executable \'$$(notdir $$@)\'...
+	@$(test) $(CXX) -o $$@ $$^ $(LDFLAGS) $(CXF_LDFLAGS)
+
+    $(cxf_target): $(cxf_deptargets) $(cxf_program)
+
+    .PHONY: _clean_$(cxf_target)
+    _clean_$(cxf_target):
+	@echo +++ [$(cxf_target)] Cleaning...
+	@$(test) $(RM) $(cxf_objfiles)
+	@$(test) $(RM) $(cxf_depfiles)
+	@$(test) $(RM) $(cxf_program)
+
+    clean:: _clean_$(cxf_target)
+	@[ -e $(CXFOUT) ] && $(test) rmdir --ignore-fail-on-non-empty $(CXFOUT)
+endef
+
+
+# each release adds another digit of i^i
+cxf_version:=0 (beta)
+version::
+	@echo "( cx-forge, version $(cxf_version) )"
+	@echo ""
+
+endif  # CXF_COMMON_INCLUDE_GUARD
--- mf-initialize.mk
+++ mf-initialize.mk
@@ -9,176 +9,22 @@
 ifndef MF_INCLUDE_GUARD
 override MF_INCLUDE_GUARD:=1
 
-mf_myname:=$(notdir $(lastword $(MAKEFILE_LIST)))
-
-# this must be defined prior
+override mfdir:=$(dir $(lastword $(MAKEFILE_LIST)))
+override mf_myname:=$(notdir $(lastword $(MAKEFILE_LIST)))
 ifndef MF_PROJECT_DIR
 $(error You must define 'MF_PROJECT_DIR' before including $(mf_myname))
 endif
 
-# superior makefiles need to be able to override this
+# superior makefiles need to be able to override MFOUT
 MFOUT:=$(MF_PROJECT_DIR)/out
-
-
-.PHONY: clean
-.PRECIOUS: $(MFOUT)/%.d $(MFOUT)/%.i
-
-
-# TODO: these should be specific to the host OS
-override MFOBJ:=o
-override MFLIB:=a
-override MFEXE:=
-
-
-ifdef TEST
-    override test:=echo
-    override pipe:=\|
-    override redir:=\>
-    override indir:=\<
-    override append:=\>\>
-else
-    override test:=
-    override pipe:=|
-    override redir:=>
-    override indir:=<
-    override append:=>>
-endif
+override CXFOUT:=$(MFOUT)
+include $(mfdir)/_cxf-common.mk
 
 
 # this will be the default make-target unless you set .DEFAULT_GOAL
 .PHONY: help
 help:
 	$(_mf_display_help)
-
-
-$(MFOUT):
-	@mkdir -p $(MFOUT)
-
-
-define mf_reset_target =
-    $(eval override mf_target:=$(1))
-    $(eval override undefine mf_srcfiles)
-    $(eval override undefine mf_objfiles)
-    $(eval override undefine mf_depfiles)
-    $(eval override undefine mf_libfiles)
-    $(eval override undefine mf_linkfiles)
-    $(eval override undefine mf_deptargets)
-    $(eval override MF_LDFLAGS:=$(LDFLAGS))
-    $(eval override MF_CPPFLAGS:=$(CPPFLAGS))
-    $(eval override MF_CXXFLAGS:=$(CXXFLAGS))
-    $(eval override undefine MF_INCLUDES)
-endef
-
-
-define _mf_compile_c++ =
-	@echo "+++ [$$(notdir $(mf_target))] $$(notdir $$<)"
-	@$(CXX) $(MF_CPPFLAGS) $(MF_INCLUDES) $(MF_CXXFLAGS) -c $$< -o $$@
-endef
-
-
-define _mf_gen_static_pattern_rule
-    $(1): $(MFOUT)/%.$(MFOBJ): $(2)/%$(3) | $(MFOUT)
-	@$(_mf_compile_c++)
-	@$(call _mf_gen_dependencies,$$*)
-endef
-
-
-define mf_add_sources =
-    $(eval override _src:=$(wildcard $(1)/$(2)))
-    $(eval override _ext:=$(suffix $(2)))
-    $(eval override _obj:=$(subst $(_ext),.$(MFOBJ),$(_src)))
-    $(eval override _obj:=$(subst $(1),$(MFOUT),$(_obj)))
-    $(eval override mf_srcfiles+= $(_src))
-    $(eval override mf_objfiles+= $(_obj))
-    $(eval $(call _mf_gen_static_pattern_rule,$(_obj),$(1),$(_ext)))
-    $(eval undefine _src)
-    $(eval undefine _ext)
-    $(eval undefine _obj)
-endef
-
-
-define _mf_import_depfiles =
-    $(eval override mf_depfiles:=$(mf_objfiles:.$(MFOBJ)=.d))
-    -include $(mf_depfiles)
-endef
-
-
-# this will generate source/header dependency info
-#    sed:      use FQPN for target
-# additionally, the following ensures that the build still succeeds
-# if source files get renamed while .d files exist:
-#    echo:     add blank line
-#    sed:      strip the target (up-to and including colon)
-#    sed:      remove any continuation backslash
-#    fmt -1:   list 'words' (files) one per line
-#    sed:      strip leading spaces
-#    sed:      add trailing colons (and blank lines)
-#    sed:      remove lines containing only a colon
-define _mf_gen_dependencies =
-	@$(test) $(CXX) -MM $(MF_CPPFLAGS) $(MF_INCLUDES) $(MF_CXXFLAGS) $$< $(redir)$(MFOUT)/$(1).d
-	@$(test) mv -f $(MFOUT)/$(1).d $(MFOUT)/$(1).d.tmp
-	@$(test) sed -e 's|.*:|$(MFOUT)/$(1).o:|' $(indir)$(MFOUT)/$(1).d.tmp $(redir)$(MFOUT)/$(1).d
-	@$(test) echo "" $(append)$(MFOUT)/$(1).d
-	@$(test) sed -e 's/.*://' \
-	             -e 's/\\$$$$//' $(indir)$(MFOUT)/$(1).d.tmp $(pipe) \
-	 $(test) fmt -1 $(pipe) \
-	 $(test) sed -e 's/^ *//' \
-	             -e 's/$$$$/:\n/' \
-	             -e 's/^:.*$$$$/\n/' $(append)$(MFOUT)/$(1).d
-	@$(test) $(RM) $(MFOUT)/$(1).d.tmp
-endef
-
-
-define mf_lib_dependency =
-    $(eval override _lib:=$(mf_$(1)_lib))
-    $(eval override mf_deptargets+= $(1))
-    $(eval override mf_linkfiles+= $(_lib))
-    $(eval undefine _lib)
-endef
-
-
-define mf_build_static_library =
-    $(eval $(_mf_import_depfiles))
-    $(eval override mf_$(mf_target)_lib:=$(MFOUT)/$(1).a)
-
-    $(mf_$(mf_target)_lib): $(mf_objfiles)
-	@echo +++ [$(mf_target)] Generating static library \'$$(notdir $$@)\'...
-	@$(AR) rcs $$@ $$^
-
-    $(mf_target): $(MF_DEPENDS) $(mf_$(mf_target)_lib)
-
-    .PHONY: _clean_$(mf_target)
-    _clean_$(mf_target):
-	@echo +++ [$(mf_target)] Cleaning...
-	@$(RM) $(mf_objfiles)
-	@$(RM) $(mf_depfiles)
-	@$(RM) $(mf_$(mf_target)_lib)
-
-    clean:: _clean_$(mf_target)
-	@[ -e $(MFOUT) ] && rmdir --ignore-fail-on-non-empty $(MFOUT)
-endef
-
-
-define mf_build_executable =
-    $(eval $(_mf_import_depfiles))
-    $(eval override mf_program:=$(MFOUT)/$(1)$(MFEXE))
-
-    $(mf_program): $(mf_objfiles) $(mf_linkfiles)
-	@echo +++ [$(mf_target)] Generating executable \'$$(notdir $$@)\'...
-	@$(CXX) -o $$@ $$^ $(LDFLAGS) $(MF_LDFLAGS)
-
-    $(mf_target): $(mf_deptargets) $(mf_program)
-
-    .PHONY: _clean_$(mf_target)
-    _clean_$(mf_target):
-	@echo +++ [$(mf_target)] Cleaning...
-	@$(RM) $(mf_objfiles)
-	@$(RM) $(mf_depfiles)
-	@$(RM) $(mf_program)
-
-    clean:: _clean_$(mf_target)
-	@[ -e $(MFOUT) ] && rmdir --ignore-fail-on-non-empty $(MFOUT)
-endef
 
 
 # .ONESHELL is needed for MF_HELPDOC, but impacts all recipes
@@ -193,12 +39,5 @@ define _mf_display_help =
 	EOF
 	@echo ""
 endef
-
-
-# each release adds another digit of i^i
-mf_version:=0 (beta)
-version::
-	@echo "( make-forge, version $(mf_version) )"
-	@echo ""
 
 endif  # MF_INCLUDE_GUARD
